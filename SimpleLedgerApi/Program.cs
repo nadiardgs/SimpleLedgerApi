c416219d180c6d6dee6a8beec9ca2a802b0d41b7
nadiardgs <nadia.rodrigues93@gmail.com>
2025-07-03 14:49:18 +0100

    feat: Configure API infrastructure services
    Integrates FluentValidation.
    Configures Swagger/OpenAPI for API documentation and interactive testing.

--- SimpleLedgerApi/Program.cs
+++ SimpleLedgerApi/Program.cs
@@ -1,17 +1,26 @@
+using FluentValidation;
+using FluentValidation.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+builder.Services.AddValidatorsFromAssemblyContaining<Program>();
+
+builder.Services.AddFluentValidationAutoValidation();
+
 builder.Services.AddOpenApi();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger(); 
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
---
+++ SimpleLedgerApi/Validators/NewTransactionRequestValidator.cs
@@ -0,0 +1,21 @@
+using FluentValidation;
+using SimpleLedgerApi.Models;
+using SimpleLedgerApi.Models.Requests;
+
+namespace SimpleLedgerApi.Validators;
+public class NewTransactionRequestValidator : AbstractValidator<NewTransactionRequest>
+{
+    public NewTransactionRequestValidator()
+    {
+        RuleFor(x => x.Amount)
+            .GreaterThan(0).WithMessage("Amount must be a positive value.");
+
+        RuleFor(x => x.Type)
+            .IsInEnum().WithMessage("Invalid transaction type specified.");
+
+        RuleFor(x => x.Description)
+            .MaximumLength(500).WithMessage("Description cannot exceed 500 characters.");
+        RuleFor(x => x.Description)
+            .NotEmpty().When(x => x.Type == TransactionType.Withdrawal).WithMessage("Description is required for withdrawals.");
+    }
+}
